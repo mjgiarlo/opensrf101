a9de71f0294c99828eadb0e490677b563b3b695b
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2010-11-08 17:08:34 -0500

    changed fixity_opensrf_http to use atomic request

--- fixity_opensrf_http.py
+++ fixity_opensrf_http.py
@@ -11,12 +11,10 @@ def fixity():
     algo = bottle.request.GET.get('algorithm')
     osrf.system.System.connect(config_file=conf_file,
                                config_context="config.opensrf")
-    session = osrf.ses.ClientSession("psu.stewardship.fixity")
-    request = session.request("psu.stewardship.fixity.generate", f, algo)
-    response = request.recv()
-    request.cleanup()
-    checksum = response.content()
-    session.cleanup()
+    checksum = osrf.ses.ClientSession.atomic_request("psu.stewardship.fixity",
+                                                     "psu.stewardship.fixity.generate",
+                                                     f,
+                                                     algo)
     return "fixity %s generated for %s" % (checksum, f)
 
 
